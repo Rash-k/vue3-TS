102862d00248df2748f67b9c792eaea550f24f8b
dyk <dyk876884846@163.com>
2021-08-03 15:07:08 +0800

    4-11

--- vue3-TS/src/components/DropDown.vue
+++ vue3-TS/src/components/DropDown.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dropdown">
+  <div class="dropdown" ref="dropDOwnRef">
     <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="dropOpen">{{title}}</a>
     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" :style="{display: 'block'}" v-show="isOpen">
       <li>
@@ -13,7 +13,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted, onUnmounted, watch } from 'vue'
+import clickOut from '@/hooks/clickOut'
 
 export default defineComponent({
   name: 'DropDown',
@@ -25,12 +26,33 @@ export default defineComponent({
   },
   setup() {
     const isOpen = ref(false)
+    const dropDOwnRef = ref<null | HTMLElement>(null)
     const dropOpen = () => {
       isOpen.value = !isOpen.value
     }
+    const isClickOutside = clickOut(dropDOwnRef)
+    watch(isClickOutside, () => {
+      if (isOpen.value && isClickOutside.value) {
+        isOpen.value = false
+      }
+    })
+    // const handler = (e: MouseEvent) => {
+    //   if (dropDOwnRef.value) {
+    //     if (!dropDOwnRef.value.contains(e.target as HTMLElement) && isOpen.value === true) {
+    //       isOpen.value = false
+    //     }
+    //   }
+    // }
+    // onMounted(() => {
+    //   document.addEventListener('click', handler)
+    // })
+    // onUnmounted(() => {
+    //   document.removeEventListener('click', handler)
+    // })
     return {
       isOpen,
-      dropOpen
+      dropOpen,
+      dropDOwnRef
     }
   }
 })
