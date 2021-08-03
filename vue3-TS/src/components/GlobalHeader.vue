aca108a007c2f1307951114f9a1049646a91e6f1
dyk <dyk876884846@163.com>
2021-08-03 14:09:33 +0800

    4-9

--- vue3-TS/src/components/GlobalHeader.vue
+++ vue3-TS/src/components/GlobalHeader.vue
@@ -6,13 +6,14 @@
       <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">注册</a></li>
     </ul>
     <ul v-else class="list-inline mb-0">
-      <li class="list-inline-item"><a href="#" class="btn btn-outline-light my-2">你好{{user.name}}</a></li>
+      <li class="list-inline-item"><drop-down :title="`你好${user.name}`"></drop-down></li>
     </ul>
   </nav>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
+import DropDown from '@/components/DropDown.vue'
 
 export interface UserProps {
   isLogin: boolean;
@@ -22,10 +23,13 @@ export interface UserProps {
 
 export default defineComponent({
   name: 'GlobalHeader',
+  components: {
+    DropDown
+  },
   props: {
     user: {
       type: Object as PropType<UserProps>,
-      require: true
+      required: true
     }
   },
   setup () {
