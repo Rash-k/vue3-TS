cc4546b745a24549096daa365e18221231fd4be5
dyk <dyk876884846@163.com>
2021-08-04 13:47:53 +0800

    5-3

--- vue3-TS/src/App.vue
+++ vue3-TS/src/App.vue
@@ -1,20 +1,43 @@
 <template>
   <div class="container">
     <global-header :user="user"></global-header>
-    <column-list :list="list"></column-list>
+<!--    <column-list :list="list"></column-list>-->
+    <form>
+      <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input :rules="emailRules"></validate-input>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input v-model="emailRef.val" @blur="validateEmail" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <div class="form-text" v-if="emailRef.error">{{emailRef.message}}</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
 import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue'
+import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue'
 
 const currentUser: UserProps = {
   isLogin: true,
   name: 'dyk'
 }
+
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 const testData: ColumnProps[] = [
   {
@@ -45,13 +68,40 @@ const testData: ColumnProps[] = [
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
-    GlobalHeader
+    // ColumnList,
+    GlobalHeader,
+    ValidateInput
   },
   setup () {
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      console.log(emailRef.val)
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = '不能为空'
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = '格式错误'
+      } else {
+        emailRef.error = false
+        emailRef.message = ''
+      }
+      return {}
+    }
     return {
       list: testData,
-      user: currentUser
+      user: currentUser,
+      emailRef,
+      validateEmail,
+      emailRules
     }
   }
 })
